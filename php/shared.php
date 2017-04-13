981dc8ad2767d63f9742af0facb013e78e428396
ashetonh <ashetoncharrell@gmail.com>
2017-04-13 16:22:55 -0400

    using mysqli and dumped query into json file
    Also var_dumped json in php as well.

--- php/shared.php
+++ php/shared.php
@@ -1,9 +1,11 @@
+
 <!--
  shared file among all php files. This is where we will put shared functions
  and connection credentials for the database.
 -->
 
 This is a table showing all sp500 stocks on 2/15/2017!
+
 <?php
 $servername = 'localhost:3306'; // change to your correct localhost port number
 $dbname = 'TermProj'; // change to your database name
@@ -12,30 +14,34 @@ $password = 'perfectmint299'; // change to your db password
 
 
 //random test 
-	try {
-	    $conn = new PDO("mysql:host=" . $GLOBALS['servername'] . ";dbname=" . $GLOBALS['dbname'], $GLOBALS['username'], $GLOBALS['password']);
-	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	    $query = "select * from sp500_quotes where date='2017-02-15'";
-;
-	    $stmt = $conn->prepare($query);
-	    $stmt->execute();
-        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        
-        echo '<table>';
-        echo '<th>ticker</th><th>date</th><th>close</th><th>volume</th>';
-        while($row = $stmt->fetch()){
-            echo '<tr>';
-            echo '<td>' . $row['ticker'] . '</td>';
-            echo '<td>' . $row['date'] . '</td>';
-            echo '<td>' . $row['close'] . '</td>';
-            echo '<td>' . $row['volume'] . '</td>';
-            echo '</tr>';        
-        } // fetching all db rows
-        
-	 
-	} catch (PDOException $e) {
-	    die('Database connection failed: ' . $e->getMessage());
-	}
-	$conn = null;
+
+$db = new mysqli($servername, $username, $password, $dbname);
+
+if($db->connect_errno > 0){
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}           
+
+$query = "select * from sp500_quotes where date='2017-02-15'";
+
+if(!$result = $db->query($query)){
+    die('There was an error running the query [' . $db->error . ']');
+}
+
+$rows = array();
+while($row = $result->fetch_assoc()){
+  $rows[] =  $row;
+
+} // fetching all db rows
+//echo json_encode($rows);
+
+mysqli_close($db);
+
+echo '</br>';
+var_dump($rows);
+//write to json file
+$fp = fopen('stocks.json', 'w');
+fwrite($fp, json_encode($rows));
+fclose($fp);
 
 ?>
+        
