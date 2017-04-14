2815a7e144093b547f51d4ec11db4b7d0c2ee177
James <jwg31957@uga.edu>
2017-04-13 20:00:49 -0400

    Added file for queries

---
+++ sqlFiles/queries.sql
@@ -0,0 +1,33 @@
+-- List of queries to implement for Term Project
+
+
+     -- Query 1: Price of ticker for a specific date
+
+     	SELECT * FROM sp500_quotes
+	WHERE date='';
+
+     -- Query 2: Price of specific ticker for a date range
+     	
+
+     -- Query 3: Ticker within a price range for a specific date
+
+
+     -- Query 4: Tickers within price range for a date range
+
+
+     -- Query 5: Average open/close price of a ticker for date range
+
+
+     -- Query 6: Stocks with greatest change in price (close) for date range
+
+
+     -- Query 7: The change in price for ticker for date range
+
+
+     -- Query 8: Show 10 companies with in certain price range (close) for specific company
+
+
+     -- Query 9: Show companies from same industry as selected ticker
+
+
+     -- Query 10: Show top 5 companies per sector based on price (close) for date
