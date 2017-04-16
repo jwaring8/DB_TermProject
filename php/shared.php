e6e860561824cae49401f77a879b23cd4b540cd5
ashetonh <ashetoncharrell@gmail.com>
2017-04-15 23:42:47 -0400

    changed shared.php

--- php/shared.php
+++ php/shared.php
@@ -4,8 +4,6 @@
  and connection credentials for the database.
 -->
 
-This is a table showing all sp500 stocks on 2/15/2017!
-
 <?php
 $servername = 'localhost:3306'; // change to your correct localhost port number
 $dbname = 'TermProj'; // change to your database name
@@ -13,7 +11,7 @@ $username = 'root'; // change to your username
 $password = 'perfectmint299'; // change to your db password 
 
 
-//random test of everything we want to use.
+//connection code
 
 $db = new mysqli($servername, $username, $password, $dbname);
 
@@ -21,21 +19,16 @@ if($db->connect_errno > 0){
     die('Unable to connect to database [' . $db->connect_error . ']');
 }           
 
-$query = "select * from sp500_quotes where ticker='AMD' order by date asc;";
+$query = "SELECT ticker, AVG(close) AS 'average closing price', YEAR(date) AS 'year' 
+            FROM sp500_quotes
+            WHERE ticker='AMD'
+            GROUP BY YEAR(date);";
 
 if(!$result = $db->query($query)){
     die('There was an error running the query [' . $db->error . ']');
 }
-$rows = array();
-while($row = mysqli_fetch_assoc($result)){
-  $rows[] =  $row;
-} // fetching all db rows
-mysqli_close($db);
-echo '</br>';
-//write to json file
-$fp = fopen('stocks.json', 'w');
-fwrite($fp, json_encode($rows, JSON_PRETTY_PRINT));
-fclose($fp);
+
+
 ?>
         
 
