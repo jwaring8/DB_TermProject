e9524210f49dfefe177061eea0603fe683237496
ashetonh <ashetoncharrell@gmail.com>
2017-04-17 03:49:41 -0400

    I DID A THING

---
+++ php/pricerange.php
@@ -0,0 +1,178 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Stock Overflow</title>
+        <script type="text/javascript" src="../d3/d3.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/3.5.6/d3.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/c3/0.4.10/c3.min.js"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="../buttons.css">
+        <link rel="stylesheet" href="../tableModel.css">
+
+
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+          <style>
+
+            /* Remove the navbar's default margin-bottom and rounded borders */ 
+            .navbar {
+              margin-bottom: 0;
+              border-radius: 0;
+            }
+
+            /* Add a gray background color and some padding to the footer */
+            footer {
+              background-color: #f2f2f2;
+              padding: 25px;
+            }
+            #buttonArea {
+
+
+            }
+            button {
+              height: 200px;
+              width: 400px;
+            }
+
+            #paragraphArea {
+              text-align: left;
+              margin-right: 10%;
+              margin-left: 10%;
+            }
+              #logo {
+                  margin-top: 3px;
+                  margin: auto;
+              }
+              #row{
+                  text-align: center;
+              }
+              #tableData {
+                  padding: 50px;
+                  background-color: #f2f2f2;
+                  border-radius: 25px;
+                  margin: 15px;
+              }
+
+
+          </style>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<!--  <link rel="stylesheet" href="/resources/demos/style.css">-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/jquery/1/jquery.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap/3/css/bootstrap.css" />
+<!--        <link rel="stylesheet" href="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css"/>-->
+        <script src="https://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
+ 
+<!-- Include Date Range Picker -->
+<script type="text/javascript" src="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.js"></script>
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/bootstrap.daterangepicker/2/daterangepicker.css" />
+ 
+
+
+<script type="text/javascript">
+$(function() {
+    $('input[name="daterange"]').daterangepicker();
+    $('input[name="daterange"]').daterangepicker({
+        locale: {
+            format: 'YYYY-MM-DD'
+        },
+        startDate: '2010-12-31',
+        endDate: '2017-03-31',
+        minDate: '2010-12-31',
+        maxDate: '2017-03-31'
+    }
+//function(start, end, label) {
+//    alert("A new date range was chosen: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+);
+});
+</script>
+    </head>
+    <body>
+        
+        <div class="jumbotron">
+          <div class="container text-center" id="logo">
+
+              <a href="../homepage.html"><img src="../images/logo.png" height="250px"></a>
+
+          </div>
+        </div>
+        <div class="container-fluid bg-3 text-center">    
+<!--              <h3>The available dates you can search are 2010-12-31 to 2017-03-31!</h3><br>-->
+              <div class="row" id="paragraphArea">
+                    <form id="formData" action ="pricerangeAJAXcall.php" method="post">
+                        <div id="datarange">Date: <input type="text" name="daterange" /></div>
+                    <div data-role="main" class="ui-content">
+                        <p>The min price is 0 and max price is 10000!</p>
+                        <div data-role="rangeslider" style="display:inline">
+                            <label for="price-min">Price-min:</label>
+                            <input type="range" name="price-min" id="price-min" value="200" min="0" max="10000">
+                            <label for="price-max">Price-max:</label>
+                            <input type="range" name="price-max" id="price-max" value="800" min="0" max="10000">
+                        </div>
+                        </div>
+                    <input type="submit" data-inline="true" value="Submit">
+                  </form>
+                  <script type="text/javascript">
+                    var frm = $('#formData');
+                    frm.submit(function (ev) {
+                        $.ajax({
+                            type: frm.attr('method'),
+                            url: frm.attr('action'),
+                            data: frm.serialize(),
+                            success: function (data) {
+                                $('#table').html(data); 
+                            }
+                        });
+
+                        ev.preventDefault();
+                    });
+                  </script>
+                  
+                  <div id="tableData"><div id="table"></div></div>                  
+              </div>
+            </div><br>
+   
+            <div class="container-fluid bg-3 text-center">    
+              <div class="row">
+                <div class="col-sm-6"> 
+                  
+                    <form action="graph.php">
+                        <button class="bttn-fill bttn-lg bttn-primary">Choose a ticker and graph!</button>
+                    </form>
+                </div>
+                <div class="col-sm-6"> 
+                  
+                    <form action="topFivePerSector.php">
+                        <button class="bttn-fill bttn-lg bttn-primary">topFivePerSector</button>
+                    </form>
+                </div>
+              </div>
+                
+                <br>
+                
+                <div class="row">
+                
+                <div class="col-sm-6">
+                  
+                    <button class="bttn-fill bttn-lg bttn-success">large</button>
+                </div>
+                <div class="col-sm-6"> 
+                  
+                    <button class="bttn-fill bttn-lg bttn-royal">large</button>
+                </div>
+
+              </div>
+            </div><br><br>
+            <footer class="container-fluid text-center">
+              <p>StockOverFlow 2017</p>
+            </footer>
+        </div>   
+    </body>
+</html>
---
+++ php/pricerangeAJAXcall.php
@@ -0,0 +1,43 @@
+<?php
+    if($_POST['daterange']!==null & $_POST['price-min']>=0 & $_POST['price-max']<=10000){
+        $daterange = $_POST['daterange'];
+        $date1 = substr($daterange, 0, 10);
+        $date2 = substr($daterange, 12);
+        $pricemin = $_POST['price-min'];
+        $pricemax = $_POST['price-max'];
+        include("shared.php");
+        $db = new mysqli($servername, $username, $password, $dbname);
+        if($db->connect_errno > 0){
+            die('Unable to connect to database [' . $db->connect_error . ']');
+        }              
+        $query = "SELECT s.ticker, s.date, s.close
+	       FROM sp500_quotes AS s
+	       WHERE (s.close BETWEEN " . $pricemin . " AND ". $pricemax . ") AND
+	      (s.date BETWEEN '". $date1 . "' AND '" . $date2 . "');";
+        
+        if(!$result = $db->query($query)){
+            die('There was an error running the query [' . $db->error . ']');
+        }
+        $row_count = $result->num_rows;
+                 $row_count = $result->num_rows;
+        if($row_count < 1){
+            echo '<h2>No data!</h2>';
+        } else{
+            echo '<table id="companies">';
+            echo '<caption>Top 5 company closing prices between ' . $date1 .' and ' .$date2 . '!</caption>';
+            echo '<th>ticker</th><th>date</th><th>close</th>';
+            while($row = $result->fetch_assoc()){
+                echo '<tr>';
+                echo '<td>' . $row['ticker'] .
+                    '</td><td>' . $row['date'] .
+                    '</td><td>' . $row['close'] . '</tr>';       
+            }
+        }
+    }
+    else{
+        
+        echo 'Missing something';
+    }
+
+
+?>
